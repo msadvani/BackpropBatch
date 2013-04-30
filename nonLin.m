4e1e74836bed11bc1bb8611c469002146a301095
Madhu <m.advani81@gmail.com>
2013-04-29 22:39:04 -0700

    Toolkit for multilayer backprop. General Nonlinearity and propagation of signal function created.
    I believe the functions work correctly, although backpropagation is not behaving as expected I expect the mistake is minor, so saving what I have so far.

---
+++ nonLin.m
@@ -0,0 +1,22 @@
+function [ y yp ] = nonLin( x )
+% Nonlinearity in a neural network
+
+beta =1; %Scaling param in nonlinearity
+
+%%Linear function
+y = beta*x;
+yp = beta*ones(size(x));
+
+
+
+%% tanh function
+% y= tanh(beta*x);
+% %derivative of y
+% yp = beta*(1-y.^2);
+
+
+%% Logistic function
+% y=1./(1+exp(-2*beta*x))
+% yp = 2*beta*y.*(1-y);
+end
+
