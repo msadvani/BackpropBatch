6885fbb0f1d471baf1576b44c2e950058504f412
Madhu <m.advani81@gmail.com>
2013-05-01 18:24:31 -0700

    Extended and fixed local noisy BP
    Extended for noisy BP over all layers and fixed the problem that I was averaging over the wrong noise and not updating

--- LocalNoisyBP.m
+++ LocalNoisyBP.m
@@ -1,13 +1,17 @@
 %Try to do this computation the simplest way to begin, by performing 3
 %seperate averages over some time interval T
+clear all;
+close all;
+
 
 %Same non-linearity g used throughout
 M = 5;   %Number of units per layer
 N = 3; %Number of layers
-T=300; %Number of timesteps to perform averaging over before recomputing weights
+T=500; %Number of timesteps to perform averaging over before recomputing weights
 epsilon=.1; %standard dev of noise
-gradStep = 1; %gradient stepSize
+gradStep = .1; %gradient stepSize
 
+layUp=[2:N] %Set of layers to update
 %Create non-linearity handle
 g = @nonLin;
 
@@ -23,59 +27,60 @@ ySoln = propSig(1,N,Wsoln,s);
 %Now we initialize the network
 W = randn(M,M,N-1);
 
-%Initialize Noise
-noise = epsilon*randn(M,N,T);
-
 %Initialize network of Neurons (for the whole time window)
 x = zeros(M,N,T);
 
 x(:,1,1) = s;
 
+noise = epsilon*randn(M,N,T);
+
 x = propNoisySig(x(:,:,1),s,W,noise,T);
 
 %Now average the appropriate quantities
 
-numIter = 1000;
+numIter = 5000;
 
 for cnt=1:numIter
-    temp = x(:,:,N);
-    x = propNoisySig(temp,s,W,noise,T);
+    
+    %Initialize parameters used to compute updates
+    dW = zeros(size(W));
+    dW_R = zeros(M,N-1);
 
+    %Run algorithm for T timesteps (and store all T)
+    noise = epsilon*randn(M,N,T);
+    x = propNoisySig(x(:,:,N),s,W,noise,T);
+    
     deltaX = mean(repmat(ySoln,[1,1,T])- x(:,N,:),3);
-    norm(deltaX)
+    norm(deltaX) %print average error from target
+    
+    
+    %Run algorithm for T timesteps (and store all T)
+    noise = epsilon*randn(M,N,T);
+    x = propNoisySig(x(:,:,N),s,W,noise,T);
 
-    c=2; %updating w_(c-1)
+    correl = zeros(M,M,N-1);
+    for c=layUp; %updating w_(c-1)
+        
+        tSet = 1:T-N+c;        
+        for t=tSet
+            correl(:,:,c-1) = correl(:,:,c-1)+ x(:,N,t+(N-c))*noise(:,c,t)';
+        end
+        correl(:,:,c-1) = correl(:,:,c-1)/length(tSet); 
 
-    tSet = 1:T-N+c;
-    correl = zeros(M,M);
-    for t=tSet
-        correl = correl+ x(:,N,t+(N-c))*noise(:,c,t)';
+        dW_R(:,c-1) = correl(:,:,c-1)'*deltaX;
     end
-    correl = correl/length(tSet); 
-
-    dW_R = correl'*deltaX;
-
-    temp = x(:,:,N);
-    x = propNoisySig(temp,s,W,noise,T);
-
-    dW = gradStep*dW_R*mean(x(:,c-1,:),3)';
-
-    W(:,:,c-1)= W(:,:,c-1)+dW;
-    %x = propNoisySig(x(:,:,N),s,W,noise,N); %push effects of new weights to the end of the network
+    
+    %Run algorithm for T timesteps (and store all T)
+    noise = epsilon*randn(M,N,T);
+    x = propNoisySig(x(:,:,N),s,W,noise,T);
+    
+    for c=layUp
+        dW(:,:,c-1) = gradStep*dW_R(:,c-1)*mean(x(:,c-1,:),3)';
+    end
+    
+    W= W+dW;
+    noise = epsilon*randn(M,N,T);
     x = propNoisySig(x(:,:,N),s,W,noise,T); %push effects of new weights to the end of the network
     
 end
-
-
-
-
-% temp = x(:,:,T);
-% x = zeros(M,N,T);
-% x(:,:,1) = temp
-% for t =2:T
-%     x(:,:,t)=propNoisySig1T(x(:,:,t-1),s,W,noise(:,:,t));
-% end
-
-
-
 
